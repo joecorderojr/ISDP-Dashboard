f296341d88cf125eb67960256b28038b8119380b
Your Name <your.email@example.com>
2026-01-15 15:12:16 +0800

    Update Sync Manager

--- Code-sync.js
+++ Code-sync.js
@@ -1,61 +1,20 @@
 function updateCyberSecurity2025() {
-  try {
-    var ss = SpreadsheetApp.openByUrl(hrURL);
-    var sheet = ss.getSheetByName("CYB101");
-    var dataArray = sheet.getRange("A3:Z600").getValues();
-    const data = filterEmptyRows(dataArray);
-
-    const headers = data[0];
-    const rows = data.slice(1);
-    const col = headers.reduce((map, h, i) => ((map[h] = i), map), {});
-
-    var objList = [];
-
-    rows.forEach((row) => {
-      const unit = row[col["BUSINESS UNIT"]];
-
-      if (!unit) return;
-
-      const inactive = row[col["INACTIVE STATUS"]];
-      const status = row[col["COURSE STATUS"]];
-      const score = parseFloat(row[col["QUIZ SCORE"]]) || null;
-      const completionDate = row[col["COMPLETION DATE"]] || null;
-
-      if (inactive === "ACTIVE") {
-        objList.push([
-          row[col["BUSINESS UNIT"]],
-          row[col["COMPLETE NAME"]],
-          row[col["POSITION TITLE"]],
-          row[col["EMAIL ADDRESS"]],
-          status,
-          completionDate,
-          score,
-        ]);
-      }
-    });
-
-    saveTrainingData(objList, "CyberSecurity2025");
-
-    let response = "Success";
-    Logger.log(response);
-    return response;
-  } catch (e) {
-    let response = "Failed to update file: " + e.toString();
-    Logger.log(response);
-    return response;
-  }
+  updateCyberSecurity("2025");
 }
 
 function updateCyberSecurity2024() {
+  updateCyberSecurity("2024");
+}
+
+function updateCyberSecurity(year) {
   try {
     var ss = SpreadsheetApp.openByUrl(hrURL);
-    var sheet = ss.getSheetByName("CYB101 2024");
+    var sheet = ss.getSheetByName("CYB101 " + year);
     var dataArray = sheet.getRange("A3:Z600").getValues();
     const data = filterEmptyRows(dataArray);
 
     const headers = data[0];
     const rows = data.slice(1);
-
     const col = headers.reduce((map, h, i) => ((map[h] = i), map), {});
 
     var objList = [];
@@ -83,7 +42,7 @@ function updateCyberSecurity2024() {
       }
     });
 
-    saveTrainingData(objList, "CyberSecurity2024");
+    saveTrainingData(objList, "CyberSecurity" + year);
 
     let response = "Success";
     Logger.log(response);
